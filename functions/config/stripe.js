6a0b9bec9ba1f65af7cc436d9ba4b83d7ede0c84
Moises Marquina <marquinaabreu@gmail.com>
2019-10-09 11:49:49 -0400

    added key in firebase and token authorization

--- functions/config/stripe.js
+++
@@ -1,6 +0,0 @@
-const keySecretSandBox = 'sk_test_xWnQERzAlyZXqy47fpDyIkXT00sqcDtynN'
-
-
-module.exports = {
-    keySecretSandBox
-}
--- functions/payments/index.js
+++ functions/payments/index.js
@@ -1,12 +1,10 @@
 const admin = require('firebase-admin');
-const keyStripe = require('../config/stripe').keySecretSandBox
+const functions = require('firebase-functions');
+const keyStripe = functions.config().stripekey.keysandbox;
 const stripe = require('stripe')(keyStripe)
 const cors = require('cors')({
     origin: true,
   })
-// const moment = require('moment')
-// const db = admin.firestore()
-
 
 const proccessPayment = (req, res) => {
     res.header('Content-Type','application/json');
@@ -17,46 +15,61 @@ const proccessPayment = (req, res) => {
     const total = (req.body.totalAmount)
     const fee = ((total *2.9)/100) +0.30
     const totalAmount = parseFloat(total + fee).toFixed(2)
-    console.log('fee ', fee)
-    console.log('Total amountt ', totalAmount)
-
-    return cors(req, res, () => {
-        stripe.customers
-        .create({
-            email: emailCustomer,
-        })
-        .then((customer) => {
-            console.log('CUSTOMERR RES ', customer)
-            return stripe.customers.createSource(customer.id, {
-            source: token,
-            });
-        })
-        .then((source) => {
-            console.log("SOURCEEE stripe res ",source)
-            return stripe.charges.create({
-            amount: totalAmount * 100,
-            currency: 'usd',
-            customer: source.customer,
-            });
-        })
-        .then((charge) => {
-            // New charge created on a new customer
-            console.log('Chargeeee ',charge)
-            return res.send({
-                success: true,
-                charge
-              })
-        })
-        .catch((err) => {
-            // Deal with an error
-            console.log('ERRROOOOOOORRR ', err)
+    const accessToken = req.header('Authorization')
+    const tokenAccess = accessToken.split(' ')
+    admin.auth().verifyIdToken(tokenAccess[1])
+      .then(async (decodedToken) => {
+        //   console.log(' decoddee ', decodedToken)
+          if( decodedToken.email === emailCustomer) {
+            return cors(req, res, () => {
+                stripe.customers
+                .create({
+                    email: emailCustomer,
+                })
+                .then((customer) => {
+                    console.log('CUSTOMERR RES ', customer)
+                    return stripe.customers.createSource(customer.id, {
+                    source: token,
+                    });
+                })
+                .then((source) => {
+                    console.log("SOURCEEE stripe res ",source)
+                    return stripe.charges.create({
+                    amount: totalAmount * 100,
+                    currency: 'usd',
+                    customer: source.customer,
+                    });
+                })
+                .then((charge) => {
+                    // New charge created on a new customer
+                    console.log('Chargeeee ',charge)
+                    return res.send({
+                        success: true,
+                        charge
+                    })
+                })
+                .catch((err) => {
+                    // Deal with an error
+                    console.log('ERRROOOOOOORRR ', err)
+                    return res.send({
+                        success: false,
+                        err
+                    })
+                });      
+            })
+          } else {
             return res.send({
                 success: false,
-                err
-              })
-        });
-      
-  })
+                err: 'Access token err'
+            })
+          }
+      }).catch( err => {
+          console.log(' error acceess ',err)
+          return res.send({
+            success: false,
+            err
+        })   
+      })
   }
 
 module.exports = {proccessPayment};
